e6e58bcb75bb240a1bc0fc5b06cb40b2c587fd09
Max Cantor <max@maxcantor.net>
2014-09-24 14:48:52 -0700

    Getting started with PlanetsController.

---
+++ app/controllers/planets_controller.rb
@@ -0,0 +1,8 @@
+class PlanetsController < ApplicationController
+
+	def index
+		# Rails does this automatically (by default)	
+		# render(:index)
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  # root 'welcome#index'
-  get '/' => 'welcome#index'
+  # the root() method...
+  # root 'planets#index'
+  # is just shorthand for this:
+  get '/'       => 'planets#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  get 'planets' => 'planets#index'
 end
