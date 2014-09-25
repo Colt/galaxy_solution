cd54ae6e09fa1cca72134275d01750b1240791f8
Max Cantor <max@maxcantor.net>
2014-09-25 13:51:43 -0700

    Dead simple CRUD and flash

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,25 +1,53 @@
 class PlanetsController < ApplicationController
 
-	def index
-		@planets = Planet.all
-	end
+    def index
+        @planets = Planet.all
+    end
 
-	def new
-	end
+    def show
+        @planet = Planet.find params[:id]
+    end
 
-	def create
-		planet = Planet.new(planet_params)
-		planet.save()
-		redirect_to planets_path
-	end
+    def new
+        @planet = Planet.new
+    end
 
-	private
+    def create
+        @planet = Planet.create planet_params
 
-	def planet_params
-		params.require(:planet).permit(
-			:name,
-			:diameter
-		)
-	end
+        flash[:notice] = "Your planet has been created."
+
+        redirect_to planets_path
+    end
+
+    def edit
+        @planet = Planet.find params[:id]
+    end
+
+    def update
+        @planet = Planet.find params[:id]
+        @planet.update planet_params
+        flash[:notice] = "Your planet has been updated."
+
+        redirect_to @planet
+    end
+
+    def destroy
+        @planet = Planet.find params[:id]
+        @planet.destroy
+
+        flash[:notice] = "Planet destroyed... you monster!"
+
+        redirect_to planets_path
+    end
+
+    private
+
+    def planet_params
+        params.require(:planet).permit(
+            :name,
+            :diameter
+        )
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  # the root() method...
-  # root 'planets#index'
-  # is just shorthand for this:
-  get '/'           => 'planets#index'
-
-  get 'planets'     => 'planets#index'
-  get 'planets/new' => 'planets#new'
-  post 'planets'    => 'planets#create'
+    root 'planets#index'
+    resources :planets
 end
